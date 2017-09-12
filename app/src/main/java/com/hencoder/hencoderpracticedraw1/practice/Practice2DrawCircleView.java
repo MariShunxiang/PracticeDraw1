42c6e63e2ca6f8bb6b233b09af2883b02d7a0be9
LuoShunXiang <marishunxiang@gmail.com>
2017-09-13 00:47:30 +0800

    add bg

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice2DrawCircleView.java
@@ -2,29 +2,45 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice2DrawCircleView extends View {
 
-    public Practice2DrawCircleView(Context context) {
-        super(context);
-    }
+  Paint mPaint;
 
-    public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+  public Practice2DrawCircleView(Context context) {
+    this(context, null);
+  }
 
-    public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+  public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs) {
+    this(context, attrs, 0);
+  }
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+  public Practice2DrawCircleView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+    init();
+  }
 
-//        练习内容：使用 canvas.drawCircle() 方法画圆
-//        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
-    }
+  private void init() {
+    mPaint = new Paint(Paint.ANTI_ALIAS_FLAG);
+  }
+
+  @Override protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+
+    //        练习内容：使用 canvas.drawCircle() 方法画圆
+    //        一共四个圆：1.实心圆 2.空心圆 3.蓝色实心圆 4.线宽为 20 的空心圆
+
+    int centerwidth = getMeasuredWidth() / 2;
+    int centerHeight = getMeasuredHeight() / 2;
+
+    mPaint.setColor(Color.BLACK);
+
+    canvas.drawCircle(centerwidth - 300, 200, 150, mPaint);
+
+  }
 }
