97a979077056d5fc1b3eca28dca735624da0ecd2
LuoShunXiang <marishunxiang@gmail.com>
2017-09-15 00:28:05 +0800

    我的天 , 最后两个图花了3个小时。而且饼图右边的线起始位置可以利用 path.rMoveTo(x,y) 优化。

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice10HistogramView.java
@@ -2,29 +2,139 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Rect;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice10HistogramView extends View {
 
-    public Practice10HistogramView(Context context) {
-        super(context);
-    }
+  Paint paint = new Paint();
 
-    public Practice10HistogramView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+  Rect textBounds = new Rect();
 
-    public Practice10HistogramView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+  String text;
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+  public Practice10HistogramView(Context context) {
+    super(context);
+  }
 
-//        综合练习
-//        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
-    }
+  public Practice10HistogramView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public Practice10HistogramView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+
+    //        综合练习
+    //        练习内容：使用各种 Canvas.drawXXX() 方法画直方图
+
+    paint.setColor(Color.WHITE);
+    paint.setAntiAlias(true);
+    paint.setStyle(Paint.Style.STROKE);
+    paint.setStrokeWidth(5);
+
+    int centerWidth = getWidth() / 2;
+    int centerHeight = getHeight() / 2;
+
+    canvas.drawLine(centerWidth - 600, centerHeight - 300, centerWidth - 600, centerHeight + 300, paint);
+    canvas.drawLine(centerWidth - 600, centerHeight + 300, centerWidth + 600, centerHeight + 300, paint);
+
+    paint.setStyle(Paint.Style.FILL);
+
+    // 矩形的底部高度
+    int bottom = centerHeight + 300;
+    // 左边的起点
+    int left = centerWidth - 600;
+
+    left += 40;
+
+    canvas.drawRect(left, bottom - 10, left + 120, bottom, paint);
+
+    paint.setColor(Color.GREEN);
+
+    left += 160;
+
+    canvas.drawRect(left, bottom - 50, left + 120, bottom, paint);
+
+    left += 160;
+
+    canvas.drawRect(left, bottom - 50, left + 120, bottom, paint);
+
+    left += 160;
+
+    canvas.drawRect(left, bottom - 250, left + 120, bottom, paint);
+
+    left += 160;
+
+    canvas.drawRect(left, bottom - 460, left + 120, bottom, paint);
+
+    left += 160;
+
+    canvas.drawRect(left, bottom - 540, left + 120, bottom, paint);
+
+    left += 160;
+
+    canvas.drawRect(left, bottom - 220, left + 120, bottom, paint);
+
+    paint.setColor(Color.WHITE);
+    paint.setTextSize(45);
+
+    text = "Froyo";
+
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+
+    int textHeight = textBounds.height(); // 计算出文字的高度
+
+    int textBottom = bottom + textHeight + 5; // 计算出文字y轴坐标
+
+    int textLeft = centerWidth - 600;
+
+    textLeft += 100; // 计算出第一个矩形的x轴方向中点
+
+    // x轴的中点减去文字的一半宽度, 得到文字起始位置
+    canvas.drawText(text, textLeft - textBounds.width() / 2, textBottom, paint);
+
+    textLeft += 160; // 计算出第二个矩形的x轴方向中点
+    text = "GB";
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+    canvas.drawText(text, textLeft - textBounds.width() / 2, textBottom, paint);
+
+    textLeft += 160;
+    text = "IC S";
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+    canvas.drawText(text, textLeft - textBounds.width() / 2, textBottom, paint);
+
+    textLeft += 160;
+    text = "JB";
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+    canvas.drawText(text, textLeft - textBounds.width() / 2, textBottom, paint);
+
+    textLeft += 160;
+    text = "KitKat";
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+    canvas.drawText(text, textLeft - textBounds.width() / 2, textBottom, paint);
+
+    textLeft += 160;
+    text = "L";
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+    canvas.drawText(text, textLeft - textBounds.width() / 2, textBottom, paint);
+
+    textLeft += 160;
+    text = "M";
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+    canvas.drawText(text, textLeft - textBounds.width() / 2, textBottom, paint);
+
+    paint.setTextSize(80);
+
+    text = "直方图";
+    paint.getTextBounds(text, 0, text.length(), textBounds);
+    canvas.drawText(text, centerWidth - textBounds.width() / 2, textBottom + 100, paint);
+  }
 }
--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice11PieChartView.java
@@ -2,29 +2,176 @@ package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
 import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
+import android.graphics.Path;
+import android.graphics.Rect;
+import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
 import android.view.View;
 
 public class Practice11PieChartView extends View {
 
-    public Practice11PieChartView(Context context) {
-        super(context);
-    }
+  Paint paint      = new Paint();
+  Paint textPaint  = new Paint();
+  Paint linePaint  = new Paint();
+  Path  path       = new Path();
+  RectF rectF      = new RectF();
+  Rect  textBounds = new Rect();
 
-    public Practice11PieChartView(Context context, @Nullable AttributeSet attrs) {
-        super(context, attrs);
-    }
+  String text;
 
-    public Practice11PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
-        super(context, attrs, defStyleAttr);
-    }
+  private static final int TEXT_DISTANCE = 20;
 
-    @Override
-    protected void onDraw(Canvas canvas) {
-        super.onDraw(canvas);
+  public Practice11PieChartView(Context context) {
+    super(context);
+  }
 
-//        综合练习
-//        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
-    }
+  public Practice11PieChartView(Context context, @Nullable AttributeSet attrs) {
+    super(context, attrs);
+  }
+
+  public Practice11PieChartView(Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+    super(context, attrs, defStyleAttr);
+  }
+
+  @Override protected void onDraw(Canvas canvas) {
+    super.onDraw(canvas);
+
+    //        综合练习
+    //        练习内容：使用各种 Canvas.drawXXX() 方法画饼图
+
+    paint.setAntiAlias(true);
+
+    textPaint.setAntiAlias(true);
+    textPaint.setColor(Color.WHITE);
+    textPaint.setStyle(Paint.Style.STROKE);
+    textPaint.setTextSize(40);
+
+    linePaint.setAntiAlias(true);
+    linePaint.setColor(Color.parseColor("#B8B8B8"));
+    linePaint.setStyle(Paint.Style.STROKE);
+    linePaint.setStrokeWidth(5);
+
+    int centerWidth = getWidth() / 2;
+    int centerHeight = getHeight() / 2;
+
+    int circleWidthCenter = centerWidth - 150;
+    int circleHeightCenter = centerHeight - 120;
+
+    int radius = Math.min(circleWidthCenter, circleHeightCenter) - 100;
+
+    /*************** 画线 **************/
+    // Lollipop
+    path.moveTo(circleWidthCenter - radius, centerHeight - radius - 70);
+    path.rLineTo(-100, 0);
+    path.moveTo(circleWidthCenter - radius, centerHeight - radius - 70);
+    path.rLineTo(50, 50);
+
+    // KitKat
+    path.moveTo(circleWidthCenter - radius + 150, circleHeightCenter + radius - 70);
+    path.rLineTo(-50, 50);
+    path.rLineTo(-150, 0);
+
+    // Marshmallow
+    path.moveTo(circleWidthCenter + radius - 50, circleHeightCenter - 100);
+    path.rLineTo(70, -70);
+    path.rLineTo(70, 0);
+
+    // 右边文字的起始位置
+    int rightTextStart = circleWidthCenter + radius - 50 + 70 + 70 + TEXT_DISTANCE;
+
+    // Froyo
+    path.moveTo(circleWidthCenter + radius, circleHeightCenter + 5);
+    path.rLineTo(rightTextStart - circleWidthCenter - radius - TEXT_DISTANCE, 0);
+
+    // Gingerbread
+    path.moveTo(circleWidthCenter + radius - 10, circleHeightCenter + 40);
+    path.rLineTo(50, 50);
+    path.rLineTo(48, 0);
+
+    // Ice Cream Sandwich
+    path.moveTo(circleWidthCenter + radius - 15, circleHeightCenter + 95);
+    path.rLineTo(55, 50);
+    path.rLineTo(48, 0);
+
+    // Jelly Bean
+    path.rMoveTo(0, 100);
+    path.rLineTo(-130, 0);
+    path.rLineTo(-50, -50);
+
+    canvas.drawPath(path, linePaint);
+
+    /*************** 画文字 **************/
+    text = "Lollipop";
+    textPaint.getTextBounds(text, 0, text.length(), textBounds);
+    int textWidth = textBounds.width();
+    canvas.drawText(text, circleWidthCenter - radius - textWidth - 100 - TEXT_DISTANCE, centerHeight - radius - 70,
+        textPaint);
+
+    text = "KitKat";
+    textPaint.getTextBounds(text, 0, text.length(), textBounds);
+    textWidth = textBounds.width();
+    canvas.drawText(text, circleWidthCenter - radius + 150 - 50 - 150 - textWidth - TEXT_DISTANCE,
+        circleHeightCenter + radius - 70 + 50, textPaint);
+
+    text = "Marshmallow";
+    canvas.drawText(text, rightTextStart, circleHeightCenter - 100 - 70, textPaint);
+
+    text = "Froyo";
+    canvas.drawText(text, rightTextStart, circleHeightCenter + 5, textPaint);
+
+    text = "Gingerbread";
+    canvas.drawText(text, circleWidthCenter + radius - 10 + 50 + 48 + TEXT_DISTANCE, circleHeightCenter + 40 + 50 + 0,
+        textPaint);
+
+    text = "Ice Cream Sandwich";
+    canvas.drawText(text, circleWidthCenter + radius - 15 + 55 + 48 + TEXT_DISTANCE, circleHeightCenter + 95 + 50 + 0,
+        textPaint);
+
+    text = "Jelly Bean";
+    canvas.drawText(text, circleWidthCenter + radius - 15 + 55 + 48 + TEXT_DISTANCE,
+        circleHeightCenter + 95 + 50 + 0 + 100, textPaint);
+
+    /*************** 画扇形 **************/
+    paint.setColor(Color.RED);
+
+    int distance = 20; // 红色圆弧离圆心距离
+
+    rectF.set(circleWidthCenter - radius - distance, circleHeightCenter - radius - distance,
+        circleWidthCenter + radius - distance, circleHeightCenter + radius - distance);
+
+    canvas.drawArc(rectF, 180, 135, true, paint);
+
+    rectF.set(circleWidthCenter - radius, circleHeightCenter - radius, circleWidthCenter + radius,
+        circleHeightCenter + radius);
+
+    paint.setColor(Color.YELLOW);
+
+    canvas.drawArc(rectF, 0, -45, true, paint);
+
+    paint.setColor(Color.parseColor("#8A2BE2")); // 紫色
+
+    canvas.drawArc(rectF, 3, 10, true, paint);
+
+    paint.setColor(Color.GRAY);
+
+    canvas.drawArc(rectF, 16, 7, true, paint);
+
+    paint.setColor(Color.parseColor("#3CB371")); // JellyBean
+
+    canvas.drawArc(rectF, 26, 50, true, paint);
+
+    paint.setColor(Color.parseColor("#436EEE"));
+
+    canvas.drawArc(rectF, 79, 101, true, paint);
+
+    /*************** 底部文字 **************/
+    textPaint.setTextSize(80);
+    text = "饼图";
+    textPaint.getTextBounds(text, 0, text.length(), textBounds);
+    textWidth = textBounds.width();
+    canvas.drawText(text, circleWidthCenter - textWidth / 2, getHeight() - 100, textPaint);
+  }
 }
