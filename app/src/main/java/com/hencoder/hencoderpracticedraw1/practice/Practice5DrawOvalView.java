de105596b1490db0bd6965e6f3abe010226da7cb
Mari <Marishunxiang@gmail.com>
2018-03-28 20:53:53 +0800

    draw path

--- app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
+++ app/src/main/java/com/hencoder/hencoderpracticedraw1/practice/Practice5DrawOvalView.java
@@ -1,12 +1,15 @@
 package com.hencoder.hencoderpracticedraw1.practice;
 
 import android.content.Context;
+import android.content.res.Resources;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.RectF;
 import android.support.annotation.Nullable;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
 import android.view.View;
 
 public class Practice5DrawOvalView extends View {
@@ -28,11 +31,28 @@ public class Practice5DrawOvalView extends View {
 
     //        练习内容：使用 canvas.drawOval() 方法画椭圆
     Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-    paint.setColor(Color.BLACK);
+    //paint.setColor(Color.BLACK);
+    //
+    //RectF rectF = new RectF(getMeasuredWidth() / 2 - 200, getMeasuredHeight() / 2 - 100, getMeasuredWidth() / 2 + 200,
+    //    getMeasuredHeight() / 2 + 100);
+    //
+    //canvas.drawOval(rectF, paint);
 
-    RectF rectF = new RectF(getMeasuredWidth() / 2 - 200, getMeasuredHeight() / 2 - 100, getMeasuredWidth() / 2 + 200,
-        getMeasuredHeight() / 2 + 100);
+    Path path = new Path();
 
-    canvas.drawOval(rectF, paint);
+    path.moveTo(0, dpToPixel(10));
+    path.lineTo(dpToPixel(7), dpToPixel(2));
+    path.lineTo(dpToPixel(116), dpToPixel(58));
+    path.lineTo(dpToPixel(116), dpToPixel(70));
+    path.lineTo(dpToPixel(7), dpToPixel(128));
+    path.lineTo(0, dpToPixel(120));
+    path.close();
+
+    canvas.drawPath(path, paint);
+  }
+
+  public static float dpToPixel(float dp) {
+    DisplayMetrics metrics = Resources.getSystem().getDisplayMetrics();
+    return dp * metrics.density;
   }
 }
